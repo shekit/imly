4ab9b460377f1a1fc2ec592ba01c8c3c85b0e8cb
Abhishek <abhishek3188@gmail.com>
2013-03-20 00:20:49 +0530

    Attempting to serve static files through s3

--- imly_project/settings.py
+++ imly_project/settings.py
@@ -77,7 +77,7 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, "static")
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = 'https://imly.s3.amazonaws.com/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
