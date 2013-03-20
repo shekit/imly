afacc83c4d688417d20a30c477fbb1d6da8f8ca8
Abhishek <abhishek3188@gmail.com>
2013-03-21 02:06:43 +0530

    Now working on production server, updating commit here

--- imly_project/settings.py
+++ imly_project/settings.py
@@ -1,6 +1,6 @@
 # Django settings for imly_project project.
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 import os
@@ -28,7 +28,7 @@ if DEBUG == True:
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(PROJECT_DIR, "imly.db"),                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(PROJECT_DIR, "database.db"),                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
