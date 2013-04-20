5f6c2c756bc0d6fa158f818460c98edb6bf40e2d
Abhishek <abhishek3188@gmail.com>
2013-04-20 19:35:37 +0530

    Corrected url issue, was shortcutting and not going to allauth urls

--- imly_project/urls.py
+++ imly_project/urls.py
@@ -15,8 +15,8 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r"^comments/", include("django_comments.urls")),
-    url(r"", include("imly.urls")),
     url(r"", include("allauth.urls")),
+    url(r"", include("imly.urls")),
 
 )
 
