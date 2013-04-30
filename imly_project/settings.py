c2ae5753c798a9b33511fda098c709519a9d5bd2
Pavan <pavanmishra@gmail.com>
2013-04-30 19:42:35 +0530

    showing number of items added to cart.

--- imly_project/settings.py
+++ imly_project/settings.py
@@ -138,6 +138,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.messages.context_processors.messages',
     'allauth.account.context_processors.account',
     'allauth.socialaccount.context_processors.socialaccount',
+    'plata.context_processors.plata_context',
 )
 
 AUTHENTICATION_BACKENDS = (
