2e8f04fe083850f0003cfafe02aa121476ff33c5
Abhishek <abhishek3188@gmail.com>
2013-05-16 17:09:53 +0530

    local settings file - abhi

--- imly_project/settings_local.py
+++ imly_project/settings_local.py
@@ -4,9 +4,9 @@ DEBUG=True # also defined in settings.py
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'igeo',                      # Or path to database file if using sqlite3.
+        'NAME': 'igo',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'igeo',
+        'USER': 'abhishek',
         'PASSWORD': '',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
