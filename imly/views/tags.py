7a04fdabc7f115feade06cc0ddf93f93880d3eca
Abhishek <abhishek3188@gmail.com>
2013-05-25 14:32:59 +0530

    Design changes and added time_choices to storeorder model to select time of pick-up/delivery

--- imly/views/tags.py
+++ imly/views/tags.py
@@ -2,11 +2,20 @@ from imly.models import Tag
 from django.shortcuts import get_object_or_404, redirect, render
 
 def add_tag(request, slug):
-    if request.session['tags']:
+    """if request.session['tags']:
         tags = request.session['tags']
         tags.append(slug)
         request.session['tags'] = tags
     else:
+        request.session['tags']=[slug]
+    return redirect(request.META["HTTP_REFERER"])"""
+
+    try:
+        request.session['tags']
+        tags = request.session['tags']
+        tags.append(slug)
+        request.session['tags'] = tags
+    except KeyError:
         request.session['tags']=[slug]
     return redirect(request.META["HTTP_REFERER"])
 
