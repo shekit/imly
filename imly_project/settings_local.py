b71e6d0a3ddd0d76b9262ed2e80739bb8076a6a7
Manish Kansara <manish.kansara07@gmail.com>
2013-05-30 19:24:16 +0530

    Data field for store.

--- imly_project/settings_local.py
+++ imly_project/settings_local.py
@@ -4,10 +4,10 @@ DEBUG=True # also defined in settings.py
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'igo',                      # Or path to database file if using sqlite3.
+        'NAME': 'igeo',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'abhishek',
-        'PASSWORD': '',
+        'USER': 'manish',
+        'PASSWORD': 'manish',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
@@ -16,4 +16,4 @@ ALLOWED_HOSTS = []
 STATIC_URL = '/static/'
 #make crispy forms fail loudly in debug mode
 CRISPY_FAIL_SILENTLY = not DEBUG
-EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
