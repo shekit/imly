be5f4b889edd5734a0304e4288b92b3cc9396bd1
Manish Kansara <manish.kansara07@gmail.com>
2013-05-31 18:39:37 +0530

    Changes.

--- imly/signals.py
+++ imly/signals.py
@@ -31,7 +31,7 @@ def set_location_point(sender,instance,**kwargs):
 #@receiver(post_save,sender=Order)
 def imly_order_place_send_email_admin(sender,instance,**kwargs):
 	if instance.status == Order.PAID and not instance.data.get('paid',''):
-		msg = EmailMessage("New Order %s Placed." % (instance._order_id),get_template('email_templates/imly_admin_order_email.html').render(Context({'order':instance,'store':[stores for stores in instance.store_set.all()]})),'orders@imly.in',['imlyfood@gmail.com'])
+		msg = EmailMessage("New Order %s Placed." % (instance._order_id),get_template('email_templates/imly_admin_order_email.html').render(Context({'order':instance})),'orders@imly.in',['imlyfood@gmail.com'])
 		msg.content_subtype = "html"
 		msg.send()
 		post_save.disconnect(imly_order_place_send_email_admin,sender=Order)
@@ -162,7 +162,7 @@ def update_product_geography(sender, instance, **kwargs):
 
 @receiver(post_save, sender=Store)
 def send_store_mail(sender,instance,created, **kwargs):
-    if created and Site.objects.get_current().domain == 'imly.in':
+    if created:# and Site.objects.get_current().domain == 'imly.in':
     	msg=EmailMessage("Store added.",get_template('email_templates/imly_store_created_admin.html').render(Context({'store':instance})),instance.owner.email,['imlyfood@gmail.com'])
     	msg.content_subtype = "html"
     	msg.send()
@@ -173,7 +173,7 @@ def send_store_mail(sender,instance,created, **kwargs):
 
 @receiver(post_save,sender=Store)
 def store_approved_email(sender,instance,created,**kwargs):
-	if instance.is_approved and not instance.data.get('email','') and Site.objects.get_current().domain == 'imly.in':
+	if instance.is_approved and not instance.data.get('email',''):# and Site.objects.get_current().domain == 'imly.in':
 		msg=EmailMessage("Store Approved.",get_template('email_templates/imly_store_confirmed.html').render(Context({'store':instance})),'imlyfood@gmail.com',[instance.owner.email])
 		msg.content_subtype = 'html'
 		msg.send()
