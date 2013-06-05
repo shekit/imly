694ac54f27a3dbbd4495f1e094e6ea74a85cd3bc
Manish Kansara <manish.kansara07@gmail.com>
2013-06-05 16:04:31 +0530

    Email template edited & admin url change.

--- imly/signals.py
+++ imly/signals.py
@@ -78,71 +78,6 @@ def set_store_order(sender,instance,**kwargs):
         store_order.store_items = instance.items.filter(product__in=store.product_set.all()).count()
         store_order.save()
 
-#@receiver(pre_save,sender=Order)
-def set_store_info(sender,instance,**kwargs):
-	stores = set((item.product.store for item in instance.items.all()))
-	instance.data['store_info'] = []
-	for store in stores:
-		instance.data['store_info'].append((
-			store.id,
-			store.name,
-			store.slug,
-			sum((item.subtotal for item in instance.items.filter(product__in=store.product_set.all()))),
-			instance.created.date() + timedelta(days=instance.items.filter(product__in=store.product_set.all()).aggregate(max=Max('product__lead_time'))['max'])
-			))
-		print instance.data
-
-#@receiver(post_save, sender=Product)
-def set_product_initial_transaction(sender,instance, created,**kwargs):
-	period = Period.objects.current()
-	if created:
-		post_save.disconnect(set_product_initial_transaction,sender=Product)
-		instance.initial_cpm = instance.capacity_per_month
-		instance.save()
-		post_save.connect(set_product_initial_transaction,sender=Product)
-		print 'post_save for initial', instance.initial_cpm
-		instance.stock_transactions.create(period=period,type=StockTransaction.INITIAL,change=instance.capacity_per_month)
-	else:
-#		print instance.capacity_per_month , StockTransaction.objects.items_in_stock(instance)
-#		if instance.capacity_per_month > StockTransaction.objects.items_in_stock(instance) :
-		sale_transaction = instance.stock_transactions.filter(type=StockTransaction.SALE).aggregate(sale_sum = Sum('change'))
-		sale_sum = sale_transaction['sale_sum']
-		#locals().update(sale_transaction)#convert the from dictionary to varible
-		if instance.initial_cpm != instance.capacity_per_month:
-			if instance.capacity_per_month > instance.initial_cpm:
-				if instance.initial_cpm <= abs(sale_sum):
-					instance.stock_transactions.create(period=period,type=StockTransaction.CORRECTION,change=instance.capacity_per_month - abs(sale_sum))					
-					instance.stock_transactions.items_in_stock(instance,update=True)	
-					post_save.disconnect(set_product_initial_transaction,sender=Product)
-					instance.initial_cpm = instance.capacity_per_month
-					instance.save()
-					post_save.connect(set_product_initial_transaction,sender=Product)
-				instance.stock_transactions.create(period=period,type=StockTransaction.CORRECTION,change=instance.capacity_per_month - instance.initial_cpm)
-				instance.stock_transactions.items_in_stock(instance,update=True)
-				post_save.disconnect(set_product_initial_transaction,sender=Product)
-				instance.initial_cpm = instance.capacity_per_month
-				instance.save()
-				post_save.connect(set_product_initial_transaction,sender=Product)	
-			else:
-				if instance.capacity_per_month > abs(sale_sum):#convert the positive value
-					instance.stock_transactions.create(period=period,type=StockTransaction.CORRECTION,change=instance.capacity_per_month - instance.initial_cpm)
-					instance.stock_transactions.items_in_stock(instance,update=True)
-					post_save.disconnect(set_product_initial_transaction,sender=Product)
-					instance.initial_cpm = instance.capacity_per_month
-					instance.save()
-					post_save.connect(set_product_initial_transaction,sender=Product)
-				else:
-					print "Capacity per month is not less than sale_transaction"
-					instance.stock_transactions.create(period=period,type=StockTransaction.CORRECTION,change= - (instance.stock_transactions.items_in_stock(instance,update=True)))
-					instance.stock_transactions.items_in_stock(instance,update=True)
-					post_save.disconnect(set_product_initial_transaction,sender=Product)
-					instance.initial_cpm = instance.capacity_per_month
-					instance.save()
-					post_save.connect(set_product_initial_transaction,sender=Product)
-		else:
-			print "Same"
-
-
 @receiver(m2m_changed, sender=Product.tags.through)
 def update_store_tags_from_product(sender, instance, action, **kwargs):
     if action == 'post_add':
@@ -166,7 +101,6 @@ def update_product_geography(sender, instance, **kwargs):
         product.save()
     post_save.connect(update_store_categories_from_product, sender=Product)
     
-
 @receiver(post_save, sender=Store)
 def send_store_mail(sender,instance,created, **kwargs):
     if created:# and Site.objects.get_current().domain == 'imly.in':
@@ -176,7 +110,6 @@ def send_store_mail(sender,instance,created, **kwargs):
     	msg=EmailMessage("Store added.",get_template('email_templates/imly_store_created_owner.html').render(Context({'store':instance})),'Imly <hello@imly.in>',[instance.owner.email])
     	msg.content_subtype = "html"
     	msg.send()
-
 
 @receiver(post_save,sender=Store)
 def store_approved_email(sender,instance,created,**kwargs):
