c2e8970167f866933b0623783ebd797db984e860
Pavan <pavanmishra@gmail.com>
2013-06-06 18:30:32 +0530

    reincluding the imly referer middleware

--- imly_project/settings.py
+++ imly_project/settings.py
@@ -114,6 +114,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'imly.middleware.RefererMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
