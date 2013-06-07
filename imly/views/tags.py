3e68e50eb88da4b313af54dba72ccd2aafce18f5
Pavan <pavanmishra@gmail.com>
2013-06-07 20:17:34 +0530

    resolved tag pagination issue and rename navigation element of chef to shop

--- imly/views/tags.py
+++ imly/views/tags.py
@@ -8,7 +8,7 @@ def add_tag(request, slug):
         request.session['tags'] = tags
     else:
         request.session['tags']=[slug]
-    return redirect(request.referer)
+    return redirect(request.referer.split('?')[0])
 
 
 def remove_tag(request, slug):
@@ -16,4 +16,4 @@ def remove_tag(request, slug):
     tag_list.remove(slug)
     request.session["tags"] = tag_list
     #raise Exception(request.session["tags"])
-    return redirect(request.referer)
+    return redirect(request.referer.split('?')[0])
