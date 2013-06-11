ca2b511d90062887869a1ef1d52481fea6061815
Pavan <pavanmishra@gmail.com>
2013-06-11 17:28:36 +0530

    working subdomain feature for cities

--- imly_project/settings.py
+++ imly_project/settings.py
@@ -1,7 +1,11 @@
 # Django settings for imly_project project.
 
 import os
-DEBUG = True
+DEBUG = False
+if not DEBUG:
+    DEPLOY_DOMAIN = 'imly.in'
+    CSRF_COOKIE_DOMAIN = SESSION_COOKIE_DOMAIN = '.' + DEPLOY_DOMAIN
+    
 TEMPLATE_DEBUG = DEBUG
 PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 LOGIN_URL="/login/"
@@ -42,7 +46,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["imly.in", 'www.imly.in']
+ALLOWED_HOSTS = ["imly.in", 'www.imly.in', '*.imly.in']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
