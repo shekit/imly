3b76bfa8d294e05f8685c1d70e24d0138b2463ad
Pavan <pavanmishra@gmail.com>
2013-06-21 18:44:18 +0530

    keeping store slug unique

--- imly/models.py
+++ imly/models.py
@@ -110,7 +110,7 @@ class Location(models.Model):
 class Store(geo_models.Model):
     #Store Details
     name = models.CharField(max_length=100)
-    slug = AutoSlugField(populate_from='name', editable=True)
+    slug = AutoSlugField(populate_from='name', editable=True, unique=True)
     owner = models.OneToOneField(User)
     store_contact_number = models.CharField(max_length=10, verbose_name="Contact Number",
                                             help_text="(Mobile number) We will not share this with anyone")
