f4d59eb3e1d435d0efba9525dbf448df4ec51aa0
Abhishek <abhishek3188@gmail.com>
2013-07-09 14:33:42 +0530

    top nav completed

--- imly/templatetags/imly_tags.py
+++ imly/templatetags/imly_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.core import urlresolvers
 from django.contrib.gis.geos import Point
 from imly.models import Category, Tag, Location, Store, Product, StoreOrder, Special
 from django.contrib.gis.measure import D
@@ -150,6 +151,26 @@ def do_store_amend_geo(parser, token):
     return StoreAmendGeo(store)
 
 register.tag('store_amend_geo', do_store_amend_geo)
+
+@register.simple_tag(takes_context=True)
+def current(context, url_name, return_value="active_top_nav", **kwargs):
+    matches = current_url_equals(context,url_name, **kwargs)
+    return return_value if matches else ""
+
+def current_url_equals(context, url_name, **kwargs):
+    resolved = False
+    try:
+        resolved = urlresolvers.resolve(context.get('request').path)
+    except:
+        pass
+    matches = resolved and resolved.url_name == url_name
+    if matches and kwargs:
+        for key in kwargs:
+            kwarg = kwargs.get(key)
+            resolved_kwarg = resolved.kwargs.get(key)
+            if not resolved_kwarg or kwarg != resolved_kwarg:
+                return False
+    return matches
 
 @register.inclusion_tag('imly/store_order_options.html')
 def store_order_options(store_order, request):
