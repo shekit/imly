38080807bec733c4b45b39e6faab879c52f2cf4f
Manish Kansara <manish.kansara07@gmail.com>
2013-07-12 20:54:07 +0530

    fixed the json ajax error.

--- imly/models.py
+++ imly/models.py
@@ -275,6 +275,7 @@ class Product(ProductBase, PriceBase, geo_models.Model):
     image_thumbnail_mini = ImageSpecField(image_field="image", format="JPEG",options={'quality': 92}, processors = [ResizeToFill(100,80)], cache_to=get_thumbnail_mini_path)
     image_thumbnail_large = ImageSpecField(image_field="image", format="JPEG",options={'quality':92}, processors = [ResizeToFit(width=575)], cache_to=get_thumbnail_large_path)
     date_created = models.DateTimeField(auto_now_add=True, editable=False)
+    updated = models.DateTimeField(auto_now=True)
     is_featured= models.BooleanField(default=False)
     is_bestseller = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag)
