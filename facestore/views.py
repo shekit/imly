73069686c3c41e46b2b47c07ae4b734cdd49c46b
Abhishek <abhishek3188@gmail.com>
2013-08-02 03:27:17 +0530

    fb product detail page and top nav designed

--- facestore/views.py
+++ facestore/views.py
@@ -33,7 +33,7 @@ def home(request):
                 products = products.exclude(special=special_event)
             except IndexError:
                 pass
-            return render(request, "facebook_store/base_facebook.html", locals())
+            return render(request, "facebook_store/fb_product_list.html", locals())
     else:
         return HttpResponse('oye, some wanderer on net, get lost you troll')
 
@@ -41,6 +41,11 @@ class FBProductDetail(DetailView):
     model = Product
     template_name = "facebook_store/fb_product_detail.html"
     
+    def get_context_data(self, **kwargs):
+        context = super(FBProductDetail,self).get_context_data(**kwargs)
+        context["store"] = self.get_object().store
+        return context
+        
     def get_queryset(self):
         store = get_object_or_404(Store,slug=self.kwargs["store_slug"])
         return store.product_set.all()
