8006880b1cf10a6c28da391bb8d2945db12cf6a8
Pavan <pavanmishra@gmail.com>
2013-08-05 21:30:22 +0530

    not allowing to create users with existing email

--- imly/patches.py
+++ imly/patches.py
@@ -1,4 +1,12 @@
 from plata.shop.forms import BaseCheckoutForm
 
 def patched_base_checkout_form_clean(self):
-    return super(BaseCheckoutForm, self).clean()
+        data = super(BaseCheckoutForm, self).clean()
+        email = data.get('email')
+        create_account = data.get('create_account')
+        if email:
+            users = list(User.objects.filter(email=email))
+            if create_account and users and self.request.user not in users:
+                self._errors['email'] = self.error_class([
+                            _('This e-mail address belongs to a different account.')])
+        return data
